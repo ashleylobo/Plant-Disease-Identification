2705c4baad12e2e6c982c8d0401b6fb96a85e8c9
kartick <kartick.hariharan@gmail.com>
2019-04-17 11:35:46 +0530

    select crop page done

--- apppppppppppp/cropPrediction/components/CropSelectionPage/index.js
+++ apppppppppppp/cropPrediction/components/CropSelectionPage/index.js
@@ -1,5 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View,Image, Text ,FlatList } from 'react-native';
+
+const routes = [
+  {name:require("../../assets/images/apple_vector.jpg"), title:"apple"},
+  {name:require("../../assets/images/corn_vector.jpg"), title:"corn"},
+  {name:require("../../assets/images/grapes_vector.jpg"), title:"grapes"},
+  {name:require("../../assets/images/peach_vector.jpg"), title:"peach"},
+  {name:require("../../assets/images/pepper_vector.jpg"), title:"pepper"},
+  {name:require("../../assets/images/potato_vector.jpg"), title:"potato"},
+  {name:require("../../assets/images/strawberry_vector.jpg"), title:"strawberry"},
+  {name:require("../../assets/images/tomato_vector.jpg"), title:"tomato"}];
+  
+const mages =  [
+    {name:1, title:"apple"},
+    {name:2, title:"corn"},
+    {name:3, title:"grapes"},
+    {name:4, title:"peach"},
+    {name:5, title:"pepper"},
+    {name:6, title:"potato"},
+    {name:7, title:"strawberry"},
+    {name:8, title:"tomato"},
+    {name:1, title:"apple"},
+    {name:2, title:"corn"},
+    {name:3, title:"grapes"},
+    {name:6, title:"potato"}];
+
 
 export default class CropSelectionPage extends Component {
   constructor(props) {
@@ -9,9 +34,31 @@ export default class CropSelectionPage extends Component {
   }
 
   render() {
+
+    for(i=1 ; i<10 ; i++){
+      
+    }
+
     return (
       <View>
-        <Text> textInComponent </Text>
+
+        <FlatList 
+          numColumns={3}
+          data = {mages}
+          
+          renderItem={i => {
+              // console.warn(routes[2].name) 
+              // console.warn(i.item.name) 
+              var queueLength = Math.floor(Math.random() * 4);
+              return (
+                  <View style={{ flex:1, flexDirection:"column", borderWidth:1, borderColor:"#e5e5e5", borderRadius:5}}>
+                    <Image style={{alignSelf:"center", width:65,height:65,margin:7}} source={i.item.name} ></Image>
+                    <Text style={{textAlign:"center", fontSize:15, marginLeft:15, marginRight:15,borderRadius:5, padding:3, backgroundColor:"#4200AE",color:"white"}}>{i.item.title}</Text>
+                  </View>
+              )}}
+        >
+      </FlatList>
+
       </View>
     );
   }
