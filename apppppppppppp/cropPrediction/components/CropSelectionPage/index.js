308006d2a347543809fdda00b679a02f17e31ef4
Suyash <suyash.sreekumar@gmail.com>
2019-04-18 09:39:00 +0530

    crop detail changes

--- apppppppppppp/cropPrediction/components/CropSelectionPage/index.js
+++ apppppppppppp/cropPrediction/components/CropSelectionPage/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { View,Image, Text ,FlatList } from 'react-native';
+import { View,Image, Text ,FlatList,Button } from 'react-native';
+import LoginSignUp from '../LoginSignUp';
 
 const routes = [
   {name:require("../../assets/images/apple_vector.jpg"), title:"apple"},
   {name:require("../../assets/images/corn_vector.jpg"), title:"corn"},
-  {name:require("../../assets/images/grapes_vector.jpg"), title:"grapes"},
+  {name:require("../../assets/images/grapes_vector.jpg"), title:"grape"},
   {name:require("../../assets/images/peach_vector.jpg"), title:"peach"},
   {name:require("../../assets/images/pepper_vector.jpg"), title:"pepper"},
   {name:require("../../assets/images/potato_vector.jpg"), title:"potato"},
@@ -12,9 +13,9 @@ const routes = [
   {name:require("../../assets/images/tomato_vector.jpg"), title:"tomato"}];
   
 const mages =  [
-    {name:1, title:"apple"},
+    {name:require("../../assets/images/apple_vector.jpg"), title:"apple"},
     {name:2, title:"corn"},
-    {name:3, title:"grapes"},
+    {name:3, title:"grape"},
     {name:4, title:"peach"},
     {name:5, title:"pepper"},
     {name:6, title:"potato"},
@@ -22,7 +23,7 @@ const mages =  [
     {name:8, title:"tomato"},
     {name:1, title:"apple"},
     {name:2, title:"corn"},
-    {name:3, title:"grapes"},
+    {name:3, title:"grape"},
     {name:6, title:"potato"}];
 
 
@@ -48,12 +49,14 @@ export default class CropSelectionPage extends Component {
           
           renderItem={i => {
               // console.warn(routes[2].name) 
-              // console.warn(i.item.name) 
+              // console.warn(i.item) 
               var queueLength = Math.floor(Math.random() * 4);
               return (
                   <View style={{ flex:1, flexDirection:"column", borderWidth:1, borderColor:"#e5e5e5", borderRadius:5}}>
                     <Image style={{alignSelf:"center", width:65,height:65,margin:7}} source={i.item.name} ></Image>
-                    <Text style={{textAlign:"center", fontSize:15, marginLeft:15, marginRight:15,borderRadius:5, padding:3, backgroundColor:"#4200AE",color:"white"}}>{i.item.title}</Text>
+                    <Button style={{textAlign:"center", fontSize:15, marginLeft:15, marginRight:15,borderRadius:5, padding:3, backgroundColor:"#4200AE",color:"white"}} title={i.item.title} onPress={()=>{
+                      this.props.navigation.navigate("cropDetailPage",{name:i.item.title})
+                    }} />
                   </View>
               )}}
         >
